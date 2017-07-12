43a35c18486aadef9148de2f2b0a7e487791ace5
semihsmg <semihsolmazgul@gmail.com>
2017-07-13 00:15:15 +0300

    v2.4

--- discord_bot.py
+++ discord_bot.py
@@ -9,9 +9,9 @@ isaret = '+'  # special character for commands
 liste_yazi = [['help', ''],
               ['correct', 'Initiating...']]
 
-# voice replies
-liste_ses = [['jarvis', r'C:/.../x.mp3', '2'],
-             ['is it that time', r'C:/.../x.mp3', '2.3']]
+# voice replies [command, dir of sound, lenght of sound in sec]
+liste_ses = [['jarvis', r'C:/.../x1.mp3', '2'],
+             ['is it that time', r'C:/.../x2.mp3', '2.3']]
 
 
 def liste_islemleri():
@@ -30,6 +30,10 @@ def isaret_ekle(liste):
 def index_bul(liste, msg):
     x = [x for x in liste if msg in x][0]
     return liste.index(x)
+
+
+def get_info(info):
+    print(str(info.user_id) + '\n' + str(datetime.datetime.now()) + '\n')
 
 
 # information about message owner
@@ -56,43 +60,57 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global komutlar
+
     # Per the discord.py docs this is to not have the bot respond to itself
     if message.author == client.user:
         return
 
-    # to transform all chars to lower case and delete whitespaces/tabs
+    info = GetInfo(message)
+    get_info(info)
+
     msg = message.content.lower().strip()
 
-    # check command message in voice answer list
-    if any(msg in s for s in liste_ses):
-        satir = index_bul(liste_ses, msg)
-        await play_audio_file(liste_ses[satir][1], GetInfo(message).channel,
-                              float(liste_ses[satir][2]), GetInfo(message).user_id)
+    if str(msg).startswith(isaret):
+        # check command message in voice answer list
+        if any(msg in s for s in liste_ses):
 
-    # check command message in written answer list
-    elif any(msg in s for s in liste_yazi):
-        satir = index_bul(liste_yazi, msg)
-        komutlar = None
+            satir = index_bul(liste_ses, msg)
+            await play_audio_file(liste_ses[satir][1], info.channel,
+                                  float(liste_ses[satir][2]))
 
-        # help funtion
-        if satir == 0 and not komutlar:
-            komutlar = 'Sesli komutlar:\n'
-            komutlar += '\n'.join(komut for i in liste_ses for komut in i if komut.startswith(isaret))
-            komutlar += '---------------\n' + 'Yazılı komutlar:\n'
-            komutlar += '\n'.join(komut for i in liste_yazi for komut in i if komut.startswith(isaret))
+        # check command message in written answer list
+        elif any(msg in s for s in liste_yazi):
 
-        if komutlar:
-            yazi = komutlar.format(message)
-            await client.send_message(message.channel, yazi)
+            satir = index_bul(liste_yazi, msg)
+            komutlar = None
 
-        if satir != 0:
-            yazi = liste_yazi[satir][1].format(message)
-            await client.send_message(message.channel, yazi)
+            # help funtion
+            if satir == 0 and not komutlar:
+                komutlar = 'Sesli komutlar:\n\n' + \
+                           '\n'.join(komut for i in liste_ses for komut in i if komut.startswith(isaret)) + \
+                           '\n------------------\n\n' + \
+                           'Yazılı komutlar:\n\n' + \
+                           '\n'.join(komut for i in liste_yazi for komut in i if komut.startswith(isaret)) + \
+                           '\n------------------\n\n' + \
+                           'Sesli komutlar için sesli sohbet kanalında bulunmalısınız.'
+
+            if komutlar:
+                yazi = komutlar.format(message)
+                await client.send_message(message.channel, yazi)
+
+            if satir != 0:
+                yazi = liste_yazi[satir][1].format(message)
+                await client.send_message(message.channel, yazi)
+
+        # if command starts with + but rest doesn't match it reply to use +help command.
+        else:
+            yanit = 'Sanırım komutunuzda sıkıntı var efendim!\n```+help``` komutunu tavsiye ederim.'.format(message)
+            await client.send_message(message.channel, yanit)
 
 
-async def play_audio_file(audio_file, get_voice_channel_id, audio_duration, id):
+async def play_audio_file(audio_file, get_voice_channel_id, audio_duration):
     try:
-        print(id, datetime.datetime.now())
         voice = await client.join_voice_channel(get_voice_channel_id)
         player = voice.create_ffmpeg_player(audio_file)
         player.start()
@@ -100,7 +118,6 @@ async def play_audio_file(audio_file, get_voice_channel_id, audio_duration, id):
         await voice.disconnect()
 
     except Exception as e:
-        print(id)
         print('Exception: ', datetime.datetime.now())
         print(e)
 
